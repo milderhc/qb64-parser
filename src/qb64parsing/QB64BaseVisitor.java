845d6191a5bef72efcaf2b6cb6d420ec75c001b3
Milder Hernandez <milderhc@gmail.com>
2017-02-26 23:45:17 -0500

    Add autogenerated folder and classes

---
+++ src/qb64parsing/QB64BaseVisitor.java
@@ -0,0 +1,329 @@
+// Generated from /home/milderhc/Documents/lenguajes/ta/antlr/qb64-parser/src/grammar/QB64.g4 by ANTLR 4.6
+package qb64parsing;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link QB64Visitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public class QB64BaseVisitor<T> extends AbstractParseTreeVisitor<T> implements QB64Visitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitQb(QB64Parser.QbContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitMain(QB64Parser.MainContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCommands(QB64Parser.CommandsContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCommand(QB64Parser.CommandContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitInput(QB64Parser.InputContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitPrint(QB64Parser.PrintContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitIf_(QB64Parser.If_Context ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitWhile_(QB64Parser.While_Context ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDo_while(QB64Parser.Do_whileContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDo_until(QB64Parser.Do_untilContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitFor_(QB64Parser.For_Context ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDeclaration(QB64Parser.DeclarationContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitId_list(QB64Parser.Id_listContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitPrint_list(QB64Parser.Print_listContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitAssignment(QB64Parser.AssignmentContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSingle_numeric_assignment(QB64Parser.Single_numeric_assignmentContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitArray_numeric_assignment(QB64Parser.Array_numeric_assignmentContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitId(QB64Parser.IdContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSingle_id(QB64Parser.Single_idContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSingle_numeric_id(QB64Parser.Single_numeric_idContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitArray_id(QB64Parser.Array_idContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitArray_numeric_id(QB64Parser.Array_numeric_idContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitArray_id_int(QB64Parser.Array_id_intContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitArray_id_long(QB64Parser.Array_id_longContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitArray_id_single(QB64Parser.Array_id_singleContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitArray_id_double(QB64Parser.Array_id_doubleContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitArray_id_string(QB64Parser.Array_id_stringContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSingle_id_int(QB64Parser.Single_id_intContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSingle_id_long(QB64Parser.Single_id_longContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSingle_id_single(QB64Parser.Single_id_singleContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSingle_id_double(QB64Parser.Single_id_doubleContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSingle_id_string(QB64Parser.Single_id_stringContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitArray(QB64Parser.ArrayContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitExpression_list(QB64Parser.Expression_listContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitExpression(QB64Parser.ExpressionContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCall_function(QB64Parser.Call_functionContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitValue(QB64Parser.ValueContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitNumeric_value(QB64Parser.Numeric_valueContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitString_value(QB64Parser.String_valueContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitType(QB64Parser.TypeContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitNegop(QB64Parser.NegopContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCompop(QB64Parser.CompopContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitBinop(QB64Parser.BinopContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitProop(QB64Parser.ProopContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitFunproc(QB64Parser.FunprocContext ctx) { return visitChildren(ctx); }
+}
---
+++ src/qb64parsing/QB64Visitor.java
@@ -0,0 +1,283 @@
+// Generated from /home/milderhc/Documents/lenguajes/ta/antlr/qb64-parser/src/grammar/QB64.g4 by ANTLR 4.6
+package qb64parsing;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link QB64Parser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface QB64Visitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#qb}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitQb(QB64Parser.QbContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#main}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitMain(QB64Parser.MainContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#commands}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCommands(QB64Parser.CommandsContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#command}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCommand(QB64Parser.CommandContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#input}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitInput(QB64Parser.InputContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#print}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPrint(QB64Parser.PrintContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#if_}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIf_(QB64Parser.If_Context ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#while_}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitWhile_(QB64Parser.While_Context ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#do_while}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDo_while(QB64Parser.Do_whileContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#do_until}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDo_until(QB64Parser.Do_untilContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#for_}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFor_(QB64Parser.For_Context ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#declaration}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDeclaration(QB64Parser.DeclarationContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#id_list}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitId_list(QB64Parser.Id_listContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#print_list}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPrint_list(QB64Parser.Print_listContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#assignment}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAssignment(QB64Parser.AssignmentContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#single_numeric_assignment}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSingle_numeric_assignment(QB64Parser.Single_numeric_assignmentContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#array_numeric_assignment}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArray_numeric_assignment(QB64Parser.Array_numeric_assignmentContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#id}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitId(QB64Parser.IdContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#single_id}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSingle_id(QB64Parser.Single_idContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#single_numeric_id}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSingle_numeric_id(QB64Parser.Single_numeric_idContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#array_id}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArray_id(QB64Parser.Array_idContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#array_numeric_id}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArray_numeric_id(QB64Parser.Array_numeric_idContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#array_id_int}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArray_id_int(QB64Parser.Array_id_intContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#array_id_long}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArray_id_long(QB64Parser.Array_id_longContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#array_id_single}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArray_id_single(QB64Parser.Array_id_singleContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#array_id_double}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArray_id_double(QB64Parser.Array_id_doubleContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#array_id_string}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArray_id_string(QB64Parser.Array_id_stringContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#single_id_int}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSingle_id_int(QB64Parser.Single_id_intContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#single_id_long}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSingle_id_long(QB64Parser.Single_id_longContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#single_id_single}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSingle_id_single(QB64Parser.Single_id_singleContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#single_id_double}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSingle_id_double(QB64Parser.Single_id_doubleContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#single_id_string}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSingle_id_string(QB64Parser.Single_id_stringContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#array}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArray(QB64Parser.ArrayContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#expression_list}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExpression_list(QB64Parser.Expression_listContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#expression}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExpression(QB64Parser.ExpressionContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#call_function}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCall_function(QB64Parser.Call_functionContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#value}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitValue(QB64Parser.ValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#numeric_value}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitNumeric_value(QB64Parser.Numeric_valueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#string_value}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitString_value(QB64Parser.String_valueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#type}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitType(QB64Parser.TypeContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#negop}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitNegop(QB64Parser.NegopContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#compop}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCompop(QB64Parser.CompopContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#binop}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBinop(QB64Parser.BinopContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#proop}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitProop(QB64Parser.ProopContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link QB64Parser#funproc}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFunproc(QB64Parser.FunprocContext ctx);
+}
